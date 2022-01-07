17dcc2732333ba29c6d324d99db3b5b6a13ba566
gcoderz <guzidemail@gmail.com>
2022-01-07 04:01:12 +0300

    settings modified

--- studybud/settings.py
+++ studybud/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-xjf26kxcaqf6==dw6ddstgmi(67q&#b$!+d$(g_e!v#^xr2n^u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['kopyastudybud-app.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
